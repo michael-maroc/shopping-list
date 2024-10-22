ec3680b4eb48408993fdb0e12c9d3a5edfcbe08e
michael <maroc.michael@hotmail.com>
2024-10-22 14:50:56 +0200

    ajout d'espaces entre les lignes après avoir entré les réponses

--- liste-de-courses.py
+++ liste-de-courses.py
@@ -10,30 +10,33 @@ while True:
             customer_choice = input(
                 "Entrez le nom d'un élément à ajouter à la liste de courses: ")
             liste_de_courses.append(customer_choice)
-            print(f"L'élément {customer_choice} à bien été ajouté à la liste.")
+            print(f"L'élément {
+                  customer_choice} à bien été ajouté à la liste.\n")
         elif int(userInput) == 2:
             customer_choice = input("Entrez le nom d'un élément à retirer: ")
             if customer_choice in liste_de_courses:
                 liste_de_courses.remove(customer_choice)
                 print(f"L'élément {
-                      customer_choice} à bien été retiré de la liste.")
+                      customer_choice} à bien été retiré de la liste.\n")
             else:
-                print("Cet élément ne figure pas dans votre liste.")
+                print(f"L'élement {
+                      customer_choice} ne figure pas dans votre liste.\n")
         elif int(userInput) == 3:
             if len(liste_de_courses) > 0:
                 print("Voici le contenu de votre liste :")
                 for i, element in enumerate(liste_de_courses):
                     i += 1
                     print(i, element)
+                print("")
             else:
-                print("Votre liste ne contient aucun élément.")
+                print("Votre liste ne contient aucun élément.\n")
         elif int(userInput) == 4:
             liste_de_courses.clear()
-            print("La liste à été vidée de son contenu.")
+            print("La liste à été vidée de son contenu.\n")
         elif int(userInput) == 5:
             print("A bientôt !")
             sys.exit()
         elif int(userInput) <= 0 or int(userInput) > 5:
-            print("Veuillez choisir un numéro entre 1 et 5")
+            print("Veuillez choisir un numéro entre 1 et 5\n")
     else:
-        print("Veuillez choisir un numéro entre 1 et 5")
+        print("Veuillez choisir un numéro entre 1 et 5\n")
