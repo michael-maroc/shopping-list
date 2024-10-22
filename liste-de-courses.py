321f21b113a230bc4973e0396144fda65c0aa7da
michael <maroc.michael@hotmail.com>
2024-10-22 15:25:34 +0200

    refactoring du code

--- liste-de-courses.py
+++ liste-de-courses.py
@@ -1,42 +1,52 @@
 import sys
 
-liste_de_courses = []
+
+LISTE = []
+
+MENU = """Choisissez parmis les 5 options suivantes : 
+1: Ajouter un élément à la liste: 
+2: Retirer un élément à la liste: 
+3: Afficher la liste 
+4: Vider la liste 
+5: Quitter 
+👉 Votre choix : """
+
+MENU_CHOICES = ["1", "2", "3", "4", "5"]
 
 while True:
-    userInput = input("Choisissez parmis les 5 options suivantes : \n1: Ajouter un élément à la liste: \n2: Retirer un élément à la liste: \n3: Afficher la liste \n4: Vider la liste \n5: Quitter \n👉 Votre choix : ")
+    userInput = input(MENU)
 
-    if userInput.isdigit():
-        if int(userInput) == 1:
-            customer_choice = input(
-                "Entrez le nom d'un élément à ajouter à la liste de courses: ")
-            liste_de_courses.append(customer_choice)
+    if userInput not in MENU:
+        print("Veuillez entrer un nombre entre 1 et 5")
+        continue
+
+    if userInput == "1":
+        customer_choice = input(
+            "Entrez le nom d'un élément à ajouter à la liste de courses: ")
+        LISTE.append(customer_choice)
+        print(f"L'élément {
+            customer_choice} à bien été ajouté à la liste.")
+    elif userInput == "2":
+        customer_choice = input("Entrez le nom d'un élément à retirer: ")
+        if customer_choice in LISTE:
+            LISTE.remove(customer_choice)
             print(f"L'élément {
-                  customer_choice} à bien été ajouté à la liste.\n")
-        elif int(userInput) == 2:
-            customer_choice = input("Entrez le nom d'un élément à retirer: ")
-            if customer_choice in liste_de_courses:
-                liste_de_courses.remove(customer_choice)
-                print(f"L'élément {
-                      customer_choice} à bien été retiré de la liste.\n")
-            else:
-                print(f"L'élement {
-                      customer_choice} ne figure pas dans votre liste.\n")
-        elif int(userInput) == 3:
-            if len(liste_de_courses) > 0:
-                print("Voici le contenu de votre liste :")
-                for i, element in enumerate(liste_de_courses):
-                    i += 1
-                    print(i, element)
-                print("")
-            else:
-                print("Votre liste ne contient aucun élément.\n")
-        elif int(userInput) == 4:
-            liste_de_courses.clear()
-            print("La liste à été vidée de son contenu.\n")
-        elif int(userInput) == 5:
-            print("A bientôt !")
-            sys.exit()
-        elif int(userInput) <= 0 or int(userInput) > 5:
-            print("Veuillez choisir un numéro entre 1 et 5\n")
-    else:
-        print("Veuillez choisir un numéro entre 1 et 5\n")
+                customer_choice} à bien été retiré de la liste.")
+        else:
+            print(f"L'élement {
+                customer_choice} ne figure pas dans votre liste.")
+    elif userInput == "3":
+        if LISTE:
+            print("Voici le contenu de votre liste :")
+            for i, element in enumerate(LISTE, 1):
+                print(f"{i}/ {element}")
+        else:
+            print("Votre liste ne contient aucun élément.")
+    elif userInput == "4":
+        LISTE.clear()
+        print("La liste à été vidée de son contenu.")
+    elif userInput == "5":
+        print("A bientôt !")
+        sys.exit()
+
+    print("-" * 50)
